c7e8f08b08725e9ab0dd065f1391bb73718aad93
Andylaa10 <andylaa100@gmail.com>
2023-03-09 11:34:24 +0100

    made a get request to see display the state of the door

--- lib/main.dart
+++ lib/main.dart
@@ -8,56 +8,65 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AdafruitData extends StatefulWidget {
+  const AdafruitData({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<AdafruitData> createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<AdafruitData> {
+  String result = '';
+
+  @override
+  void initState() {
+    getSensorData();
+    //startTimer();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    PostRequest post = PostRequest();
-    return MaterialApp(
-      title: 'Smart Noor Lock',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Smart Door Lock', style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),)),
       ),
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: SizedBox(
-                  height: 200,
-                  width: 200,
-                  child: FloatingActionButton(
-                    child: Text('Open', style: TextStyle(fontSize: 20, color: Colors.black),),
-                    onPressed: ()=> post.postData('1'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Center(
+              child: Text(
+                result,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: SizedBox(
+                height: 200,
+                width: 200,
+                child: FloatingActionButton(
+                  child: Text(
+                    'Open',
+                    style: TextStyle(fontSize: 20, color: Colors.black),
                   ),
+                  onPressed: () {
+                    postData('1');
+                  },
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: SizedBox(
-                  height: 200,
-                  width: 200,
-                  child: FloatingActionButton(
-                    child: Text('Close', style: TextStyle(fontSize: 20, color: Colors.black),),
-                    onPressed: ()=> post.postData('0'),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
-}
-
-class PostRequest {
 
   //dashboard
   //https://io.adafruit.com/MonoDevice/dashboards/smart-noor-lock
@@ -75,31 +84,76 @@ class PostRequest {
     final String jsonString = await rootBundle.loadString('assets/config.json');
     final jsonMap = json.decode(jsonString);
     String apiKey = jsonMap['api_Key'];
-    String url = jsonMap['http_URL'];
+    String url = jsonMap['http_URL_Lock'];
     //print(apiKey);
     //print(url);
 
-    final headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer $apiKey'};
-    final body = {"datum": {"value": value}};
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $apiKey'
+    };
+    final body = {
+      "datum": {"value": value}
+    };
 
-    final response = await http.post(Uri.parse(url), headers: headers, body: jsonEncode(body));
+    final response = await http.post(Uri.parse(url),
+        headers: headers, body: jsonEncode(body));
 
     if (response.statusCode == 200) {
       // handle success
-      print(response.body);
+      await getSensorData();
     } else {
       // handle error
+      //print('Error: ${response.statusCode}');
+    }
+  }
+
+  Future<void> getSensorData() async {
+    final String jsonString = await rootBundle.loadString('assets/config.json');
+    final jsonMap = json.decode(jsonString);
+    String url = jsonMap['get_latest_value'];
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      String lastValue = jsonResponse['last_value'];
+      print(lastValue);
+      print(jsonResponse);
+      if (lastValue == '1') {
+        setState(() {
+          result = 'Door is Open';
+        });
+      } else if (lastValue == '0') {
+        setState(() {
+          result = 'Door is Locked';
+        });
+      }
+    } else {
+      //handle error
       print('Error: ${response.statusCode}');
     }
   }
+
+  void startTimer() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      getSensorData();
+    });
+  }
 }
 
-/**
- * This is an exam project in Internet of things (IOT),
- * which is one of the choosen elective courses on 4th semester.
- * We have made a smart door lock, which sends information to flespi mqtt and adafruit io.
- * Adafruit io controls the smart door lock, and flespi mqtt receives data based on the state of the door lock.
- * As a nice to have we have made a fairly simple flutter app which sends post request to adafruit io,
- * so the state of the smart door lock can be changed through the app.
- */
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Noor Lock',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AdafruitData(),
+    );
+  }
+}
